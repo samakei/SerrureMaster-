d84cb3010033a9ea938fe135dca99cacea584863
Samake Issa <samakeissa10@outlook.fr>
2025-12-31 04:03:43 +0100

    ci: ESLint flat config  disable inline config to avoid unknown rule errors

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ import globals from 'globals';
 export default [
   {
     ignores: ['**/build/**', '**/dist/**', '**/node_modules/**'],
-    linterOptions: { reportUnusedDisableDirectives: 'off' },
+    linterOptions: { reportUnusedDisableDirectives: 'off', noInlineConfig: true },
   },
   js.configs.recommended,
   {
